c8de447e165467bd70374cc0ee26fe6a5e55771b
Hasan Hüseyin GÜNEŞ <hgunes@RPK.LOCAL>
2021-08-24 16:01:50 +0300

    api_versioning

---
+++ SampleAPI/SampleAPI/Controllers/v1/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAPI.Controllers.v1
+{
+    [Route("api/v{version:apiVersion}/users")]
+    [ApiController]
+    [ApiVersion("1.0",Deprecated =true)]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult AllUsers()
+        {
+            List<UserV1> users = new List<UserV1>()
+            {
+                new UserV1
+                {
+                    Id=1,
+                    Name="asya"
+                },
+                new UserV1
+                {
+                    Id=2,
+                    Name="asya2"
+                },
+                new UserV1
+                {
+                    Id=3,
+                    Name="asya3"
+                },
+            };
+
+            return Ok(users);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ SampleAPI/SampleAPI/Controllers/v2/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAPI.Controllers.v2
+{
+    [Route("api/users")] //https://localhost:44371/api/users?api-version=2.0
+   // [ApiController]
+    //[ApiVersion("2.0")]
+   // [Route("api/v{version:apiVersion}/users")] //https://localhost:44371/api/v2/users
+    [ApiVersion("2.0")]
+    public class UsersController : ControllerBase
+    {
+      
+        [HttpGet()]
+        public IActionResult AllUsers()
+        {
+            List<UserV2> users = new List<UserV2>()
+            {
+                new UserV2
+                {
+                    Id=1,
+                    Name="hasan"
+                },
+                new UserV2
+                {
+                    Id=2,
+                    Name="hasan"
+                },
+                new UserV2
+                {
+                    Id=3,
+                    Name="hasan"
+                },
+            };
+
+            return Ok(users);
+        }
+    }
+}
